ddf63530adae6d0cdaf476806bfd425e9cf86568
Nasibov Rafael <nasibovrafael1@gmail.com>
2024-07-17 16:49:08 +0300

    auth removed

---
+++ Client/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import {Component, inject, OnInit} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'SchoolsLMS';
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
